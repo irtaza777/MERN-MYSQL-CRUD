0e0384b05dfb1e6e11f1c68c0c9f432a83475205
irtaza777 <irtazarizvi7@gmail.com>
2024-05-14 21:42:01 +0500

    again

--- src/components/User-Status/userstatus.js
+++ src/components/User-Status/userstatus.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const Userstatus = () => {
-    return (  <div><h1>Branch</h1></div>);
+    return (  <div><h1> My New Branchs</h1></div>);
 }
  
 export default Userstatus;
